9654e534688f0d8146a4f5d56a14b1dff83242c1
Ammar Ratnani <ammrat13@gmail.com>
2020-03-10 00:35:56 -0400

    Started stepper arduino code

---
+++ stepper_arduino/stepper_arduino.ino
@@ -0,0 +1,88 @@
+#include <AccelStepper.h>
+
+
+// For serial
+#define SERIAL_TIMEOUT (1)
+
+// Stepper motor constants
+#define STEPPER_MAX_SPEED (150)
+#define STEPPER_MAX_ACCEL (STEPPER_MAX_SPEED*STEPPER_MAX_SPEED)
+
+// For the enabled button
+#define ENABLED_DEBOUNCE_T (200)
+
+
+// Pin setup
+#define PIN_EN_DETECT (3)
+#define PIN_EN_LED (49)
+
+#define PIN_ST0 (50)
+#define PIN_ST1 (51)
+#define PIN_ST2 (52)
+#define PIN_ST3 (53)
+
+
+boolean enabled = false;
+unsigned long enabledDebounce = 0;
+
+AccelStepper stepper(AccelStepper::FULL4WIRE, PIN_ST0, PIN_ST1, PIN_ST2, PIN_ST3, true);
+
+
+/* Handlers to take care of interrupts when the button is pushed */
+
+void enableRisingHandler() {
+    // Just log the debounce time if it changes in the wrong direction
+    enabledDebounce = millis();
+}
+
+void enableFallingHandler() {
+    // Check for debounce time
+    if(millis() > enabledDebounce + ENABLED_DEBOUNCE_T) {
+        // Toggle enable and change LED
+        enabled = !enabled;
+        digitalWrite(PIN_EN_LED, enabled ? HIGH : LOW);
+    }
+
+    // Remember to log the debounce time
+    enabledDebounce = millis();
+}
+
+
+void setup()  {
+    Serial.begin(115200);
+    Serial.setTimeout(SERIAL_TIMEOUT);
+    
+    // Create the handler for enable
+    pinMode(PIN_EN_DETECT, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(PIN_EN_DETECT), enableRisingHandler, RISING);
+    attachInterrupt(digitalPinToInterrupt(PIN_EN_DETECT), enableFallingHandler, FALLING);
+
+    // For the enabled LED
+    pinMode(PIN_EN_LED, OUTPUT);
+    digitalWrite(PIN_EN_LED, LOW);
+
+    // Stepper setup
+    stepper.setMaxSpeed(STEPPER_MAX_SPEED);
+    stepper.setSpeed(STEPPER_MAX_SPEED);
+    stepper.setAcceleration(STEPPER_MAX_ACCEL);
+}
+
+void loop() {
+    char buf[64];
+    if(Serial.readBytesUntil('\n', buf, 64)) {
+        // We only ever get the target position on Serial
+        // We know what we need to so
+        stepper.move(atoi(buf));
+
+        // Print out the result in format <DTG,Enabled>
+        Serial.print("<");
+        Serial.print(stepper.distanceToGo());
+        Serial.print(",");
+        Serial.print(enabled ? 1 : 0);
+        Serial.print(">");
+        Serial.println();
+    }
+
+    // Don't forget to run the stepper
+    stepper.run();
+}
